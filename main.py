b0a37beba7b5f332c0392b18ca76b82f7bad12cd
maximus <maximus.sallam@gmail.com>
2021-12-19 07:25:12 +0800

    25:10 into tutorial.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
+
+
+class BoxLayoutExample(BoxLayout):
+    pass
 
 
 class MainWidget(Widget):
